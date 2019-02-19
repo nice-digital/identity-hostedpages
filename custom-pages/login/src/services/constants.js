eb766ebf5b99c927e90fda5d96c553704ea97d06
AlzAmido <alessio.fimognari@amido.com>
2019-02-19 16:39:06 +0000

    changing url

--- custom-pages/login/src/services/constants.js
+++ custom-pages/login/src/services/constants.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 export const auth = Object.freeze({
-  domain: 'dev-nice-identity.auth0.com',
+  domain: 'alpha-nice-identity.eu.auth0.com',
   clientID: 'RMpYVjKE_B85IO6Euc2i3IstVAstrEHK',
   scope: 'openid profile email'
 })
