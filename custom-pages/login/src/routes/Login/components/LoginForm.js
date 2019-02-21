2c6f831549965d80d9a41b453ef1972a521fa2d0
AlzAmido <alessio.fimognari@amido.com>
2019-02-21 10:54:07 +0000

    should do

--- custom-pages/login/src/routes/Login/components/LoginForm.js
+++ custom-pages/login/src/routes/Login/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import auth0 from 'auth0-js'
 import qs from 'qs'
-import CordovaAuth0Plugin from 'auth0-js/dist/cordova-auth0-plugin.min'
+// import CordovaAuth0Plugin from 'auth0-js/dist/cordova-auth0-plugin.min'
 // import Logo from '../assets/logo.png'
 import { auth } from '../../../services/constants'
 import classes from './LoginForm.css'
@@ -12,15 +12,14 @@ export class Login extends React.Component {
     this.opts = {
       domain: auth.domain,
       clientID: auth.clientID,
-      plugins: [new CordovaAuth0Plugin()],
-      leeway: 1,
-      audience: '',
-      responseMode: 'query',
-      popup: false,
+      // plugins: [new CordovaAuth0Plugin()],
+      // leeway: 1,
+      // audience: '',
+      // responseMode: 'query',
+      // popup: false,
       responseType: auth.responseType,
       scope: auth.scope,
-      redirect: true,
-      realm: 'Username-Password-Authentication',
+      // redirect: true
     }
     this.redirectUri = null
     this.state = {
@@ -32,17 +31,20 @@ export class Login extends React.Component {
   componentDidMount() {
     const query = qs.parse(document.location.search, { ignoreQueryPrefix: true })
     this.redirectUri = query.redirect_uri ? query.redirect_uri : document.location.origin
-    this.auth0 = new auth0.WebAuth(this.opts)
+    const config = document.location.origin.includes('localhost') ? {} : JSON.parse(decodeURIComponent(escape(window.atob('@@config@@'))))
+    config.extraParams = config.extraParams || {}
+    const params = Object.assign(this.opts, config.internalOptions)
+    this.auth0 = new auth0.WebAuth(params)
   }
 
   login = (e) => {
     e.preventDefault()
     const { username, password } = this.state
     console.log(username, password, this.redirectUri)
-    // this.auth0.authorize(
-    this.auth0.login(
+    this.auth0.authorize(
+    // this.auth0.login(
       {
-        ...this.opts,
+        // realm: 'Username-Password-Authentication',
         redirectUri: this.redirectURI,
         username: 'alessio.fimognari@amido.com',
         password: 'Password01!'
