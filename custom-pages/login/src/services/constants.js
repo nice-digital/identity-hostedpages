dd3a1d1758dec93864c13dc7cf342e428505403e
AlzAmido <alessio.fimognari@amido.com>
2019-02-21 15:39:36 +0000

    adding octo vars

--- custom-pages/login/src/services/constants.js
+++ custom-pages/login/src/services/constants.js
@@ -1,8 +1,17 @@
 // eslint-disable-next-line
+// export const auth = Object.freeze({
+//   domain: 'alpha-nice-identity.eu.auth0.com',
+//   clientID: 'ETzPLUtLTkCs8tHDjBfxNJKnnUzQGlmf',
+//   scope: 'openid profile email',
+//   responseType: 'code',
+//   connection: 'Username-Password-Authentication'
+// })
+
+// eslint-disable-next-line
 export const auth = Object.freeze({
-  domain: 'alpha-nice-identity.eu.auth0.com',
-  clientID: 'ETzPLUtLTkCs8tHDjBfxNJKnnUzQGlmf',
+  domain: '#{AUTH0_DOMAIN}',
+  clientID: '#{AUTH0_CLIENT_ID}',
   scope: 'openid profile email',
   responseType: 'code',
-  connection: 'Username-Password-Authentication'
+  connection: '#{AUTH0_CONNECTION}'
 })
