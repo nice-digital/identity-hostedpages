0266cc00b9f7fafe458d6b4492c531c90dbe2df8
AlzAmido <alessio.fimognari@amido.com>
2019-03-06 09:21:01 +0000

    typo

--- custom-pages/login/tools/bundle.password_reset.js
+++ custom-pages/login/tools/bundle.password_reset.js
@@ -4,7 +4,7 @@
  *
  * Copyright © 2015-2016 Konstantin Tarkus (@koistya)
  *
- * This source code is licensed under the MIT license found in the  
+ * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
 
@@ -12,15 +12,18 @@ const webpack = require('webpack')
 const task = require('./task')
 const webpackConfig = require('../webpack.config.password_reset')
 
-module.exports = task('bundle.password_reset', new Promise((resolve, reject) => {
-  const bundler = webpack(webpackConfig)
-  const run = (err, stats) => {
-    if (err) {
-      reject(err)
-    } else {
-      console.log(stats.toString(webpackConfig.stats))
-      resolve()
+module.exports = task(
+  'bundle.password_reset',
+  new Promise((resolve, reject) => {
+    const bundler = webpack(webpackConfig)
+    const run = (err, stats) => {
+      if (err) {
+        reject(err)
+      } else {
+        console.log(stats.toString(webpackConfig.stats))
+        resolve()
+      }
     }
-  }
-  bundler.run(run)
-}))
+    bundler.run(run)
+  })
+)
