704336a81ff37959afd7bebff6315f6977300b48
AlzAmido <alessio.fimognari@amido.com>
2019-03-19 11:45:42 +0000

    removed message and link from resetpasswordsuccess page

--- custom-pages/login/src/routes/ResetPasswordSuccess/ResetPasswordSuccess.js
+++ custom-pages/login/src/routes/ResetPasswordSuccess/ResetPasswordSuccess.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Alert from '@nice-digital/nds-alert'
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 
 // local imports
 import { hideNav } from '../../util'
@@ -15,12 +15,11 @@ export const ResetPasswordSuccess = () => {
     <div>
       <h3>Thank you!</h3>
       <Alert type="success">
-        Your password has been changed and you will need to sign in with it, if
-        this page does not automatically refresh{' '}
-        <Link data-qa-sel="resetPasswordSuccess-link-to-login" to="/login">
+        Your password has been changed and you can now use it to sign in {' '}
+        {/* <Link data-qa-sel="resetPasswordSuccess-link-to-login" to="/login">
           {' '}
           click here{' '}
-        </Link>
+        </Link> */}
       </Alert>
     </div>
   )
