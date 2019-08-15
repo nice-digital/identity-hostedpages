c2da0e553cdf9e9e1c7ea1af1eafcde0eb271ef9
Jose Hilario Almeida <josealmeida@gmail.com>
2019-08-15 10:55:50 +0100

    IDAM-256 Refactor routes for partial match

--- custom-pages/universal-login/src/components/App/App.js
+++ custom-pages/universal-login/src/components/App/App.js
@@ -25,13 +25,14 @@ class App extends React.Component {
                     <Route exact path="/" component={Login} /> :
                     <Route exact path="/" component={ResetPassword} />
                   }
-                  <Route exact path='/login' component={Login} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/regsuccess' component={RegisterSuccess} />
-                  <Route exact path='/forgotPassword' component={ForgotPassword} />
-                  <Route exact path='/forgotsuccess' component={ForgotPasswordSuccess} />
-                  <Route exact path='/resetpassword' component={ResetPassword} />
-                  <Route exact path='/resetsuccess' component={ResetPasswordSuccess} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/register' component={Register} />
+                  <Route path='/regsuccess' component={RegisterSuccess} />
+                  <Route path='/forgotPassword' component={ForgotPassword} />
+                  <Route path='/forgotsuccess' component={ForgotPasswordSuccess} />
+                  <Route path='/resetpassword' component={ResetPassword} />
+                  <Route path='/change-password' component={ResetPassword} />
+                  <Route path='/resetsuccess' component={ResetPasswordSuccess} />
                   <Route component={NotFound} />
                 </Switch>
             </Router>
