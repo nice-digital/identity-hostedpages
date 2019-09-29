73e1e57fa32c4419315535a874635c6c84f35439
John Williams <jcwilliams@gmail.com>
2019-09-29 13:54:09 +0100

    IDAM-256 - undoing the extra route

--- custom-pages/universal-login/src/components/App/App.js
+++ custom-pages/universal-login/src/components/App/App.js
@@ -31,7 +31,6 @@ class App extends React.Component {
                   <Route path='*/forgotPassword' component={ForgotPassword} />
                   <Route path='*/forgotsuccess' component={ForgotPasswordSuccess} />
                   <Route path='*/resetpassword' component={ResetPassword} />
-                  <Route path='*/lo/reset' component={ResetPassword} />
                   <Route path='*/change-password' component={ResetPassword} />
                   <Route path='*/resetsuccess' component={ResetPasswordSuccess} />
                   <Route component={NotFound} />
