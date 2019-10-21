fe20b81331edea6db4ffaf500d7795dd01cf11fa
John Williams <jcwilliams@gmail.com>
2019-10-21 11:50:32 +0100

    IDAM-284 - fixing a bug in the setting of the error variable

--- custom-pages/universal-login/src/components/Login/Login.js
+++ custom-pages/universal-login/src/components/Login/Login.js
@@ -125,7 +125,8 @@ class Login extends Component {
       showGoogleLogin,
       activationEmailSent
     } = this.state
-    const { showUserNotVerfiedMessage } = (this.querystring && this.querystring.myerrorcode === 'user_not_verified');
+
+    const showUserNotVerfiedMessage = (this.querystring && this.querystring.myerrorcode === 'user_not_verified');
 
     return (
       <div>
