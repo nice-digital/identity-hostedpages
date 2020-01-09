b284d0bf2b852e23dfdbd62a715f6d32f7186d19
John Holland <john.holland@nice.org.uk>
2020-01-09 16:00:17 +0000

    IDAM-289 Email templates for verify and reset pw

---
+++ custom-pages/emails/gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const css = require('gulp-inline-css');
+
+function compileSCSS() {
+    return src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./src/css'));
+}
+
+function inlineCSS() {
+    return src('./src/*.html')
+        .pipe(css({
+            preserveMediaQueries: true,
+            removeStyleTags: false
+        }))
+        .pipe(dest('./'));
+}
+
+exports.compileSCSS = compileSCSS;
+exports.inlineCSS = inlineCSS;
+exports.default = function() {
+    watch(['./src/scss/**/*.scss', './src/*.html'], { ignoreInitial: false }, series(compileSCSS, inlineCSS));
+};
