71faa44705113206bc76a592f0a7d8f9606e90a6
Sue Garner <Sue.Garner@nice.org.uk>
2020-07-27 15:01:43 +0100

    IDAM-183 renamed tempCid1 to temp_cid

--- custom-pages/universal-login/src/services/AuthApi.js
+++ custom-pages/universal-login/src/services/AuthApi.js
@@ -90,7 +90,7 @@ export default class AuthApi {
           realm: connection,
           username,
           password,
-          tempCid1: tempCid
+          temp_cid: tempCid
         }
         method = 'login'
       } else {
