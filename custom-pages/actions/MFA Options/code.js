356e3e114958df689da8a3daa2b82f426879f547
Cheguegallagher <martin.gallagher@nice.org.uk>
2025-04-24 17:05:34 +0100

    IDAM-636 Add application exclusions to mfa options action

--- custom-pages/actions/MFA Options/code.js
+++ custom-pages/actions/MFA Options/code.js
@@ -7,23 +7,27 @@
  * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
  */
 
-  
 exports.onExecutePostLogin = async (event, api) => {
-  console.log("hello la" + JSON.stringify(event));
-   if ((event.connection.strategy !== "waad" && 
-       event.connection.strategy !== "google-oauth2")){
-    const enrolledFactors = event.user.enrolledFactors.map((f) => ({type: f.type}));
-    if (enrolledFactors.length < 2)
-    {
-      api.authentication.enrollWithAny([{type: 'otp'}]);
-    } else {
-      api.authentication.challengeWith({ type: 'otp' }, 
-        { additionalFactors: enrolledFactors })
+  if (!event.secrets.client_ids.includes(event.client.client_id)) {
+    if (
+      event.connection.strategy !== 'waad' &&
+      event.connection.strategy !== 'google-oauth2'
+    ) {
+      const enrolledFactors = event.user.enrolledFactors.map((f) => ({
+        type: f.type,
+      }));
+      if (enrolledFactors.length < 2) {
+        api.authentication.enrollWithAny([{ type: 'otp' }]);
+      } else {
+        api.authentication.challengeWith(
+          { type: 'otp' },
+          { additionalFactors: enrolledFactors }
+        );
+      }
+      return;
     }
-    return;
-  }  
+  }
 };
-
 
 /**
  * Handler that will be invoked when this action is resuming after an external redirect. If your
