44da3a85fd350cd4d8db18c209da22c12f47504f
John Holland <john.holland@nice.org.uk>
2025-07-21 15:37:30 +0100

    IDAM-626 Fix for merge conflicted file

--- custom-pages/universal-login/src/helpers/__tests__/validateRegisterFields.test.js
+++ custom-pages/universal-login/src/helpers/__tests__/validateRegisterFields.test.js
@@ -1,89 +1,102 @@
-import { validateRegisterFields } from '../index'
+import { validateRegisterFields } from "../index";
 
-describe('validateFields ', () => {
-  it('should return tests.email => true with invalid email', () => {
-    const emailValidation = validateRegisterFields({ email: 'invalidEmail' }).email()
-    expect(emailValidation).toBe(true)
-  })
-  it('should return tests.email => falsey with valid email', () => {
+describe("validateFields ", () => {
+  it("should return tests.email => true with invalid email", () => {
+    const emailValidation = validateRegisterFields({ email: "invalidEmail" }).email();
+    expect(emailValidation).toBe(true);
+  });
+
+  it("should return tests.email => falsey with valid email", () => {
     const emailValidation = validateRegisterFields({
-      email: 'validEmail@provider.com'
-    }).email()
-    expect(emailValidation).toBe(false)
-  })
-  it('should return tests.email => falsey with no email', () => {
-    const emailValidation = validateRegisterFields({}).email()
-    expect(emailValidation).toBe(undefined)
-  })
+      email: "validEmail@provider.com"
+    }).email();
+    expect(emailValidation).toBe(false);
+  });
+
+  it("should return tests.email => falsey with no email", () => {
+    const emailValidation = validateRegisterFields({}).email();
+    expect(emailValidation).toBe(undefined);
+  });
+
 
   // password
-  it('should return tests.password => true with invalid password (only numbers)', () => {
-    const passwordValidation = validateRegisterFields({ password: '65340598673045986711' }).password()
-    expect(passwordValidation).toBe(true)
-  })
-  it('should return tests.password => true with invalid password (only chars)', () => {
-    const passwordValidation = validateRegisterFields({ password: 'sdklfhgabslkhdjfgalshjdkfb' }).password()
-    expect(passwordValidation).toBe(true)
-  })
-  it('should return tests.password => true with invalid password (no uppercase and no special chars)', () => {
-    const passwordValidation = validateRegisterFields({ password: 'testwaefasdfgsaed1234325452' }).password()
-    expect(passwordValidation).toBe(true)
-  })
-  it('should return tests.password => true with invalid password (too short)', () => {
-    const passwordValidation = validateRegisterFields({ password: 'Test12.' }).password()
-    expect(passwordValidation).toBe(true)
-  })
-  it('should return tests.password => falsey with valid password', () => {
-    const passwordValidation = validateRegisterFields({
-      password: 'P@ssw0rd1234!&A'
-    }).password()
-    expect(passwordValidation).toBe(false)
-  })
-  it('should return tests.password => falsey with no password', () => {
-    const passwordValidation = validateRegisterFields({}).password()
-    expect(passwordValidation).toBe(undefined)
+  it("should return tests.password => true with invalid password (only numbers)", () => {
+    const passwordValidation = validateRegisterFields({ password: "65340598673045986711" }).password();
+    expect(passwordValidation).toBe(true);
+  });
+
+  it("should return tests.password => true with invalid password (only chars)", () => {
+    const passwordValidation = validateRegisterFields({ password: "sdklfhgabslkhdjfgalshjdkfb" }).password();
+    expect(passwordValidation).toBe(true);
+  });
+
+  it("should return tests.password => true with invalid password (no uppercase and no special chars)", () => {
+    const passwordValidation = validateRegisterFields({ password: "testwaefasdfgsaed1234325452" }).password();
+    expect(passwordValidation).toBe(true);
+  });
+
+  it("should return tests.password => true with invalid password (too short)", () => {
+    const passwordValidation = validateRegisterFields({ password: "Test12." }).password();
+    expect(passwordValidation).toBe(true);
   })
 
-  it('should return tests.confirmPassword => falsey with no password', () => {
+  it("should return tests.password => falsey with valid password", () => {
+    const passwordValidation = validateRegisterFields({
+      password: "P@ssw0rd1234!&A"
+    }).password();
+    expect(passwordValidation).toBe(false);
+  });
+
+  it("should return tests.password => falsey with no password", () => {
+    const passwordValidation = validateRegisterFields({}).password();
+    expect(passwordValidation).toBe(undefined);
+  })
+
+  it("should return tests.confirmPassword => falsey with no password", () => {
     const confirmPasswordValidation = validateRegisterFields({
-      confirmPassword: 'anypassword'
-    }).confirmPassword()
-    expect(confirmPasswordValidation).toBe(undefined)
-  })
-  it('should return tests.confirmPassword => true for invalid confirmPassword', () => {
+      confirmPassword: "anypassword"
+    }).confirmPassword();
+    expect(confirmPasswordValidation).toBe(undefined);
+  });
+
+  it("should return tests.confirmPassword => true for invalid confirmPassword", () => {
     const confirmPasswordValidation = validateRegisterFields({
-      password: 'P@ssw0rd1234!&A',
-      confirmPassword: 'P@ssw0rd1234!&'
-    }).confirmPassword()
-    expect(confirmPasswordValidation).toBe(true)
-  })
-  it('should return tests.confirmPassword => falsey with no password and no confirmPassword', () => {
-    const confirmPasswordValidation = validateRegisterFields({}).confirmPassword()
-    expect(confirmPasswordValidation).toBe(undefined)
-  })
-  // it('should return tests.confirmPassword => true with no confirmPassword', () => {
+      password: "P@ssw0rd1234!&A",
+      confirmPassword: "P@ssw0rd1234!&"
+    }).confirmPassword();
+    expect(confirmPasswordValidation).toBe(true);
+  });
+
+  it("should return tests.confirmPassword => falsey with no password and no confirmPassword", () => {
+    const confirmPasswordValidation = validateRegisterFields({}).confirmPassword();
+    expect(confirmPasswordValidation).toBe(undefined);
+  });
+
+  // it("should return tests.confirmPassword => true with no confirmPassword", () => {
   //   const confirmPasswordValidation = validateRegisterFields({
-  //     password: 'Password01!'
-  //   }).confirmPassword()
-  //   expect(confirmPasswordValidation).toBe(true)
-  // })
+  //     password: "Password01!"
+  //   }).confirmPassword();
+  //   expect(confirmPasswordValidation).toBe(true);
+  // });
 
   // name and surname
-  it('should return tests.name => true with invalid name', () => {
-    const nameValidation = validateRegisterFields({ name: 'thisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongname' }).name()
-    expect(nameValidation).toBe(true)
-  })
-  it('should return tests.name => true with valid name', () => {
-    const nameValidation = validateRegisterFields({ name: 'clark' }).name()
-    expect(nameValidation).toBe(false)
-  })
+  it("should return tests.name => true with invalid name", () => {
+    const nameValidation = validateRegisterFields({ name: "thisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongnamethisisaverylongname" }).name();
+    expect(nameValidation).toBe(true);
+  });
 
-  it('should return tests.surname => true with invalid surname', () => {
-    const surnameValidation = validateRegisterFields({ surname: 'thisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurname' }).surname()
-    expect(surnameValidation).toBe(true)
-  })
-  it('should return tests.surname => true with valid surname', () => {
-    const surnameValidation = validateRegisterFields({ surname: 'kent' }).surname()
-    expect(surnameValidation).toBe(false)
-  })
-})
+  it("should return tests.name => true with valid name", () => {
+    const nameValidation = validateRegisterFields({ name: "clark" }).name();
+    expect(nameValidation).toBe(false);
+  });
+
+  it("should return tests.surname => true with invalid surname", () => {
+    const surnameValidation = validateRegisterFields({ surname: "thisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurnamethisisaverylongsurname" }).surname();
+    expect(surnameValidation).toBe(true);
+  });
+
+  it("should return tests.surname => true with valid surname", () => {
+    const surnameValidation = validateRegisterFields({ surname: "kent" }).surname();
+    expect(surnameValidation).toBe(false);
+  });
+});
